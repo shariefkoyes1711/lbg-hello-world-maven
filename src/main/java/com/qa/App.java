83af1e3cfe526c9cdccafacbe221544d73b10f91
skoyes <sharief.koyes@lloydsbanking.com>
2024-12-03 14:00:49 +0000

    added aline for hello to myself 1

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(SayHelloToSomeone("Sharief"));
+        System.out.println(sayHelloToSomeone("Sharief"));
         System.out.println(sayGoodbye());
     }
 
